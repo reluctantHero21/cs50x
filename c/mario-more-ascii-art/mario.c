fd913f1c199fb610e3ab13e5945cf221510c48bc
Tony Liu <tony.k.liu@outlook.com>
2022-06-11 14:09:59 -0400

    Update mario.c
    Replaced for loop variable name by 'row' for enhanced readability
    Removed unnecessary blank spaces

--- c/mario-more-ascii-art/mario.c
+++ c/mario-more-ascii-art/mario.c
@@ -1,4 +1,4 @@
-// https://cs50.harvard.edu/x/2021/psets/1/mario/more/
+// https://cs50.harvard.edu/x/2022/psets/1/mario/more/
 
 #include <cs50.h>
 #include <stdio.h>
@@ -16,26 +16,24 @@ int main(void)
     }
     while (height < 1 || height > 8);
 
-    // Print pyramid
-
-    for (int i = 0; i < height; i++)
+    // Print pyramid, row by row
+    for (int row = 0; row < height; row++)
     {
         //print blank space
-        print_space(height - i - 1);
+        print_space(height - row - 1);
 
         //print hashtag (left half)
-        print_hashtag(i + 1);
+        print_hashtag(row + 1);
 
         //print 2 spaces
         printf("  ");
 
         //print hastag (right half)
-        print_hashtag(i + 1);
+        print_hashtag(row + 1);
 
         //print line break
         printf("\n");
     }
-
 }
 
 //helper function that prints n number of "#"
